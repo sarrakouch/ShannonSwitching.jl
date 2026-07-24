f4c393fd49cb1b0b644d8c4efc20e1995bc63a61
Sarra Kouch <sarrakouche1@gmail.com>
2026-07-24 17:25:17 +0200

    types
    Define structures for Vertex, Edge, GameGraph, and GameState.

--- types.jl
+++ types.jl
@@ -1,1 +1,26 @@
 #just the struct definitions
+struct Vertex
+    id::Int
+end
+
+mutable struct Edge
+    id::Int
+    u::Vertex
+    v::Vertex
+    weight::Float64
+    state::Symbol
+end
+
+struct GameGraph
+    vertices::Vector{Vertex}
+    edges::Vector{Edge}
+    s::Vertex
+    t::Vertex
+end
+
+mutable struct GameState
+    graph::GameGraph
+    current_player::Symbol
+    history::Vector{Tuple{Symbol, Edge}}
+    winner::Union{Symbol, Nothing}
+end
